46bd0a4708f750d525c1deb55d1182927d73a855
HastiGarjani <117920223+HastiGarjani@users.noreply.github.com>
2025-05-15 08:34:54 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+
+CONFIG_DICT = {
+    "Logistic6": {
+        "ro_min": 0.001,
+        "ro_max": 4,
+        "ro_initial" : 1,
+        "aRS_min": 0.1,
+        "aRS_max": 15,
+        "aRS_initial" : 2,
+        "aSR_min": 0.001,
+        "aSR_max": 8,
+        "aSR_initial" : 2,
+        "K_min": 15000,
+        "K_max":3250000,
+        "K_initial": 200000,
+        "lambda_min": 0.1,
+        "lambda_max": 15,
+        "lambda_initial" : 2,
+    }
+    }
+
+
+
